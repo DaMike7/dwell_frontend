0230504059c17f6b53e5117e3edfb9936a54d56e
DaMike7 <michaeldabere462@gmail.com>
2024-05-11 23:05:37 +0100

    updated api.js

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true
 
 const client = axios.create({
-  baseURL: `${process.env.BACKEND_URL}`
+  baseURL: 'https://dwellhomes.vercel.app'
 });
 
 
